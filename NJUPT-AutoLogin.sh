473c8c0c5fddb5dbee1ab00c8c64c832d5df55e3
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-04-17 10:23:02 +0800

    Refactorize and add macOS support

--- NJUPT-AutoLogin.sh
+++ NJUPT-AutoLogin.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+#     _   _     _ _   _ ____ _____
+#    | \ | |   | | | | |  _ \_   _|
+#    |  \| |_  | | | | | |_) || |
+#    | |\  | |_| | |_| |  __/ | |
+#    |_| \_|\___/ \___/|_|    |_|
+#
+#        _         _        _                _
+#       / \  _   _| |_ ___ | |    ___   __ _(_)_ __
+#      / _ \| | | | __/ _ \| |   / _ \ / _` | | '_ \
+#     / ___ \ |_| | || (_) | |__| (_) | (_| | | | | |
+#    /_/   \_\__,_|\__\___/|_____\___/ \__, |_|_| |_|
+#                                      |___/
+#
+#    Example of usage:
+#    bash NJUPT-AutoLogin.sh -i en0 -o ctcc -t 2 B21012250 12345678
+#
 #     _   _             _______  _
 #    |  \| | _   _   ___  | |    _   __ _  _ __
 #    | . ` || | | | / _ \ | |   | | / _` || '_ \
@@ -7,282 +23,343 @@
 #
 #    Author: NuoTian (https://github.com/s235784)
 #    Repository: https://github.com/s235784/NJUPT_AutoLogin
-#    Version: 1.1.2
+#    Version: 1.1.3 (Refactorized and added macOS support by BlockLune)
 
-# 脚本使用格式 如bash NJUPT-AutoLogin.sh -e eth0.2 -i ctcc -l B21012250 12345678
+# ANSI color and style codes
+BOLD=$(printf '\033[1m')
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+RESET=$(printf '\033[0m')
 
-# eth口
-eth="eth0.1"
+login_id=""
+login_pw=""
+operator="ctcc"
+interface="en0"
+timeout=2
+time_limited_account=0
+logout_flag=1
 
-# 运营商 校园网为njupt，电信为ctcc，移动为cmcc
-isp="njupt"
+println_error() {
+	local error_message="$1"
+	printf "%s%sERROR%s:\t%s\n" "$BOLD" "$RED" "$RESET" "$error_message"
+}
 
-# 是否只能在规定时间内联网
-limit="false"
+println_ok() {
+	local ok_message="$1"
+	printf "%s%sOKAY%s:\t%s\n" "$BOLD" "$GREEN" "$RESET" "$ok_message"
+}
 
-# 仙林校区
-wlanacip="10.255.252.150"
-
-wlanacname="XL-BRAS-SR8806-X"
-
-# 忽略未插入网线的错误状态
-ignore_disconnet="false"
-
-logout_flag="false"
+println_warning() {
+	local warning_message="$1"
+	printf "%s%sWARN%s:\t%s\n" "$BOLD" "$YELLOW" "$RESET" "$warning_message"
+}
+println_info() {
+	local info_message="$1"
+	printf "%s%sINFO%s:\t%s\n" "$BOLD" "$BLUE" "$RESET" "$info_message"
+}
 
 help() {
-  echo "登录命令："
-  echo "NJUPT-AutoLogin.sh [-e eth] [-i isp] [-p ip] [-l] [-s] [-d] username password"
-  echo "登出命令："
-  echo "NJUPT-AutoLogin.sh [-e eth] [-p ip] [-s] -o"
-  echo "参数描述："
-  echo "eth，路由器ETH口"
-  echo "isp，运营商 校园网为njupt，电信为ctcc，移动为cmcc"
-  echo "ip，手动指定向登录接口发送的IP地址"
-  echo "l，仅在规定时间内尝试自动登录"
-  echo "s，三牌楼校区须添加此参数"
-  echo "d，忽略网线未插入的错误状态"
-  echo "username，账号"
-  echo "password，密码"
-  exit 0
+	printf "Auto login script for NJUPT Wi-Fi.\n"
+	printf "Author: NuoTian (https://github.com/s235784)"
+	printf "\n"
+	printf "Usage: %s [-i interface] [-o operator] [-t timeout] [-p ipv4_addr] [-m] [-n] [-h] login_id login_password\n" "$0"
+	printf "Options:\n"
+	printf "\t-i interface\tSpecify the network interface. Default is '%s'.\n" "$interface"
+	printf "\t-o operator\tSpecify the operator. Default is '%s'.\n" "$operator"
+	printf "\t-t timeout\tSpecify the timeout for ping. Default is %d seconds.\n" "$timeout"
+	printf "\t-p ipv4_addr\tSpecify the IPv4 address. By default it will be detacted automatically.\n"
+	printf "\t-m\t\tSwitch to logout mode. Default is %d (0 for ON, 1 for OFF).\n" "$logout_flag"
+	printf "\t-n\t\tSwitch to not time limited account. Default is %d (0 for LIMITED, 1 for NOT).\n" "$time_limited_account"
+	printf "\t-h\t\tShow this help message.\n"
+	printf "Arguments:\n"
+	printf "\tlogin_id\tThe user ID.\n"
+	printf "\tlogin_password\tThe user password.\n"
+	exit 0
 }
 
-# 获取设备IP地址
-ip() {
-  if [ ! "$ip" ]
-	then
-    ip=$(ifconfig "${eth}" | grep "inet " | awk '{print $2}' | tr -d "addr:")
-    if [ ! "$ip" ]
-  	then
-  		printf "获取ip地址失败\n"
-  		exit 0
-    fi
-	fi
-  printf "当前设备的ip地址为${ip}\n"
-}
-
-# 退出登录
-logout() {
-  ip
-  result=$(curl -k --request GET "https://10.10.244.11:802/eportal/portal/logout?callback=dr1003&login_method=1&user_account=drcom&user_password=123&wlan_user_ip=${ip}&&wlan_user_ipv6=&waln_vlan_id=0&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=" \
-  --connect-timeout 5 \
-  --interface ${eth})
-  printf "\n"
-  if [[ $result =~ "成功" ]]
-  then
-    printf "已退出校园网登录\n"
-  else
-    printf "接口请求错误：${result}\n如果问题持续出现，请在GitHub上提交Issue\n"
-  fi
-  exit 0
-}
-
-while getopts 'e:i:p:lsdoh' OPT; do
-    case $OPT in
-        e) eth="$OPTARG";;
-        i) isp="$OPTARG";;
-        p) ip="$OPTARG";;
-        l) limit="true";;
-        s) wlanacip="10.255.253.118"
-           wlanacname="SPL-BRAS-SR8806-X";;
-        d) ignore_disconnet="true";;
-        o) logout_flag="true";;
-        h) help;;
-        ?) help;;
-    esac
-done
-
-if [ "$logout_flag" = "true" ]
-then
-  ip
-  logout
-fi
-
-shift $(($OPTIND - 1))
-
-# 账号
-name=$1
-
-# 密码
-passwd=$2
-
-echo "eth口：$eth"
-echo "运营商：$isp"
-echo "账号：$name"
-echo "密码：$passwd"
-echo "账号是否会断网：$limit"
-
-if [ "$ip" ]
-then
-  echo "指定的IP地址：$ip"
-fi
-
-echo ""
-
-# 检测网络连接畅通
-network()
-{
-  status=$(curl --interface ${eth} -s -m 2 -IL baidu.com)
-  # 请求直接返回空
-  if [ "$status" = "" ]
-  then
-    printf "无法判断网络状态，尝试登录\n"
-    return 0
-  fi
-
-  http_code=$(echo "${status}"|grep "200")
-  connection=$(echo "${status}"|grep "close")
-
-  if [ "$http_code" = "" ]
-  then
-    printf "网络已断开\n"
-
-    if [ "${ignore_disconnet}" = "true" ]
-    then
-      printf "已设置忽略该错误，继续登录命令\n"
-      if [ "$connection" = "" ]
-      then
-        # 网络通畅
-        return 1
-      else
-        # 未登录
-        return 0
-      fi
-    fi
-
-    exit 0
-  fi
-
-  if [ "$connection" = "" ]
-  then
-    # 网络通畅
-    return 1
-  else
-    # 未登录
-    return 0
-  fi
-}
-
-# 登录校园网
-loginNet() {
-	if [ ! "$eth" ]
-	then
-		printf "eth不能为空\n"
-		exit 0
-	fi
-
-	if [ ! "$isp" ]
-	then
-		printf "运营商不能为空\n"
-		exit 0
-	fi
-
-	if [ ! "$name" ]
-	then
-		printf "用户名不能为空\n"
-		exit 0
-	fi
-
-	if [ ! "$passwd" ]
-	then
-		printf "密码不能为空\n"
-		exit 0
-	fi
-
-	ip
-
-	if [ "$isp" = "ctcc" ]
-	then
-	   printf "运营商为电信\n"
-		 login="%2C0%2C${name}%40njxy"
-	elif [ "$isp" = "cmcc" ]
-	then
-	   printf "运营商为移动\n"
-		 login="%2C0%2C${name}%40cmcc"
-	elif [ "$isp" = "njupt" ]
-	then
-	   printf "运营商为校园网\n"
-		 login="%2C0%2C${name}"
-	 else
-		 printf "无法识别运营商\n"
-		 exit 0
-	fi
-
-	result=$(curl -k --request GET "https://10.10.244.11:802/eportal/portal/login?callback=dr1003&login_method=1&user_account=${login}&user_password=${passwd}&wlan_user_ip=${ip}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=" \
-  --connect-timeout 5 \
-  --interface ${eth})
-  printf "\n"
-  if [[ $result =~ "成功" ]]
-  then
-    printf "已成功登录校园网\n"
-  else
-    printf "接口请求错误：${result}\n如果问题持续出现，请在GitHub上提交Issue\n"
-  fi
-}
-
-# 开始登录
-start() {
-	network
-	if [ $? -eq 0 ]
-	then
-		loginNet
+turn_on_wifi_and_connect_mac() {
+	println_info "Trying to trun on Wi-Fi..."
+	networksetup -setairportpower "$interface" on
+	if [[ "$operator" == "ctcc" ]]; then
+		println_info "Your operator is China Telecom."
+		networksetup -setairportnetwork "$interface" "NJUPT-CHINANET"
+	elif [[ "$operator" == "cmcc" ]]; then
+		println_info "Your operator is China Mobile."
+		networksetup -setairportnetwork "$interface" "NJUPT-CMCC"
+	elif [[ "$operator" == "njupt" ]]; then
+		println_info "Your operator is NJUPT."
+		networksetup -setairportnetwork "$interface" "NJUPT"
 	else
-		printf "网络正常，无需登录\n"
+		println_error "Invalid operator. Please specify 'ctcc', 'cmcc' or 'njupt'."
+		exit 1
 	fi
 }
 
-# 检查当前时间
-checkTime() {
-  week=$(date +%w)
-  time=$(date +%H%M)
-
-  # 周一至周四
-  if [ "$week" -ge 1 ] && [ "$week" -le 4 ]
-  then
-  	# 7：01到23：29之间
-  	if [ "$time" -ge 0701 ] && [ "$time" -le 2329 ]
-  	then
-  			printf "允许时间内，开始准备登录\n"
-  			start
-    else
-        printf "不在允许时间内\n"
-  	fi
-  # 周五
-  elif [ "$week" -eq 5 ]
-  then
-  	# 7：01之后
-  	if [ "$time" -ge 0701 ]
-  	then
-  			printf "允许时间内，开始准备登录\n"
-  			start
-    else
-        printf "不在允许时间内\n"
-  	fi
-  # 周六全天
-  elif [ "$week" -eq 6 ]
-  then
-  	printf "允许时间内，开始准备登录\n"
-  	start
-  # 周日
-  elif [ "$week" -eq 0 ]
-  then
-  	# 23：29之前
-  	if [ "$time" -le 2329 ]
-  	then
-  			printf "允许时间内，开始准备登录\n"
-  			start
-    else
-        printf "不在允许时间内\n"
-  	fi
-  fi
+turn_on_wifi_and_connect_linux() {
+	println_info "Trying to trun on Wi-Fi..."
+	if [[ "$operator" == "ctcc" ]]; then
+		println_info "Your operator is China Telecom."
+    nmcli dev wifi connect "NJUPT-CHINANET"
+	elif [[ "$operator" == "cmcc" ]]; then
+		println_info "Your operator is China Mobile."
+		nmcli dev wifi connect "NJUPT-CMCC"
+	elif [[ "$operator" == "njupt" ]]; then
+		println_info "Your operator is NJUPT."
+		nmcli dev wifi connect "NJUPT"
+	else
+		println_error "Invalid operator. Please specify 'ctcc', 'cmcc' or 'njupt'."
+		exit 1
+	fi
 }
 
-if [ "$limit" = "false" ]
-then
-  printf "没有时间限制，开始准备登录\n"
-  start
-else
-  checkTime
-fi
+# TODO: It's better to also check ssid
+# TODO: to make sure the specific Wi-Fi is connected.
+is_wifi_on() {
+	local result
+  if [[ "$(uname)" == "Darwin" ]]; then
+    result=$(networksetup -getairportpower "$interface" | grep "On")
+  elif [[ "$(uname)" == "Linux" ]]; then
+    result=$(nmcli radio wifi | grep "enable")
+  fi
+	if [[ -n $result ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
 
-printf "完成\n"
+# the prefix `get` means you can get the value by using $()
+get_ip_address() {
+	local ip_type="$1"
+	if [ "$ip_type" == "ipv4" ]; then
+		ifconfig "$interface" | grep 'inet ' | awk '{print $2}' | head -n 1
+	elif [ "$ip_type" == "ipv6" ]; then
+		ifconfig "$interface" | grep 'inet6 ' | grep -v 'fe80::' | awk '{print $2}' | head -n 1
+	else
+		println_error "Invalid IP type. Please specify 'ipv4' or 'ipv6'."
+		return 1
+	fi
+}
+
+# the prefix `generate` means you can't get the value by using $()
+# the values are stored in global variables
+# TODO: This function gets both ipv4 and ipv6 addresses.
+# TODO: However the login API currently only use ipv4 address.
+# TODO: There is an option in the API to use ipv6 address,
+# TODO: called `wlan_user_ipv6`.
+# TODO: So the ipv6 address may be used in the future.
+generate_ip_addresses() {
+	println_info "Trying to obtain IP addresses..."
+
+	# get ipv4 address
+	ipv4_addr=$(get_ip_address "ipv4")
+	if [[ -n $ipv4_addr ]]; then
+		println_ok "IPv4 address ($ipv4_addr) obtain successfully!"
+	else
+		println_warning "IPv4 address not found on $interface!"
+	fi
+
+	# get ipv6 address
+	ipv6_addr=$(get_ip_address "ipv6")
+	if [[ -n $ipv6_addr ]]; then
+		println_ok "IPv6 address ($ipv6_addr) obtain successfully!"
+	else
+		println_warning "IPv6 address not found on $interface!"
+	fi
+
+	if [[ -z $ipv4_addr ]] && [[ -z $ipv6_addr ]]; then
+		println_error "No IP addresses found on $interface!"
+		exit 1
+	fi
+}
+
+generate_user_account() {
+	println_info "Generating user_account..."
+	if [[ "$operator" == "ctcc" ]]; then
+		println_info "Your operator is China Telecom."
+		user_account="%2C0%2C${login_id}%40njxy"
+	elif [[ "$operator" == "cmcc" ]]; then
+		println_info "Your operator is China Mobile."
+		user_account="%2C0%2C${login_id}%40cmcc"
+	elif [[ "$operator" == "njupt" ]]; then
+		println_info "Your operator is NJUPT."
+		user_account="%2C0%2C${login_id}"
+	else
+		println_error "Invalid operator. Please specify 'ctcc', 'cmcc' or 'njupt'."
+		exit 1
+	fi
+	println_ok "user_account ($user_account) generated."
+}
+
+generate_user_password() {
+	println_info "Generating user_password..."
+	user_password=$login_pw
+	println_ok "user_password ($user_password) generated."
+}
+
+login_the_wifi() {
+	generate_user_account
+	generate_user_password
+	if [[ -z $ipv4_addr ]]; then
+		generate_ip_addresses
+	else
+		println_info "Using the specified IPv4 address: $ipv4_addr"
+	fi
+	response=$(curl --interface "$interface" -s -k --request GET "https://10.10.244.11:802/eportal/portal/login?callback=dr1003&login_method=1&user_account=$user_account&user_password=$user_password&wlan_user_ip=$ipv4_addr")
+	if [[ "$response" =~ "成功" ]]; then
+		println_ok "Login successful!"
+	else
+		println_error "Login failed!"
+		println_info "Response: %s\n" "$response"
+	fi
+}
+
+logout_the_wifi() {
+	response=$(curl --interface "$interface" -s -k --request GET "https://10.10.244.11:802/eportal/portal/logout?callback=dr1003&login_method=1&wlan_user_ip=$ipv4_addr")
+
+	if [[ "$response" =~ "成功" ]]; then
+		println_ok "Logout successful!"
+	else
+		println_error "Logout failed!"
+		printf "Response: %s\n" "$response"
+	fi
+}
+
+check_time() {
+	local week
+	week=$(date +%w)
+
+	local time
+	time=$(date +%H%M)
+
+	# Monday to Thursday
+	if [[ "$week" -ge 1 ]] && [[ "$week" -le 4 ]]; then
+		# 7:01 to 23：29
+		if [[ "$((10#$time))" -ge 701 ]] && [[ "$((10#$time))" -le 2329 ]]; then
+			return 0
+		fi
+		# Friday
+	elif [[ "$week" -eq 5 ]]; then
+		# After 7:01
+		if [[ "$((10#$time))" -ge 701 ]]; then
+			return 0
+		fi
+		# Saturday
+	elif [[ "$week" -eq 6 ]]; then
+		return 0
+		# Sunday
+	elif [[ "$week" -eq 0 ]]; then
+		# Before 23:29
+		if [[ "$((10#$time))" -le 2329 ]]; then
+			return 0
+		fi
+	fi
+
+	return 1
+}
+
+is_internet_connectable() {
+	ping -c 1 -W "$timeout" baidu.com &>/dev/null
+}
+
+main() {
+  if [[ "$(uname)" != "Darwin" ]] && [[ "$(uname)" != "Linux" ]]; then
+		println_error "This script is not running on macOS or Linux."
+		exit 1
+	fi
+
+  if [[ "$(uname)" == "Darwin" ]] ; then
+    println_ok "This script is running on macOS."
+  elif [[ "$(uname)" == "Linux" ]]; then
+    println_ok "This script is running on Linux."
+  fi
+
+	if [[ $logout_flag -eq 0 ]]; then
+		logout_the_wifi
+		exit 0
+	fi
+
+	println_info "Parsing the options..."
+	println_info "User ID: ${BOLD}$login_id${RESET}"
+	println_info "User Password: ${BOLD}$login_pw${RESET}"
+	println_info "Operator: ${BOLD}$operator${RESET}"
+	println_info "Interface: ${BOLD}$interface${RESET}"
+	if [[ -n $ipv4_addr ]]; then
+		println_info "IPv4 Address: ${BOLD}$ipv4_addr${RESET}"
+	fi
+	if [[ $time_limited_account -eq 0 ]]; then
+		println_info "Account Type: ${BOLD}LIMITED${RESET}"
+	else
+		println_info "Account Type: ${BOLD}NOT LIMITED${RESET}"
+	fi
+	if [[ $logout_flag -eq 0 ]]; then
+		println_info "Logout flag: ${BOLD}ON${RESET}"
+	else
+		println_info "Logout flag: ${BOLD}OFF${RESET}"
+	fi
+
+	if is_wifi_on; then
+		println_ok "Wi-Fi is on."
+	else
+		println_info "Trying to turn on Wi-Fi..."
+    if [[ "$(uname)" == "Darwin" ]] ; then
+      turn_on_wifi_and_connect_mac
+    elif [[ "$(uname)" == "Linux" ]]; then
+      turn_on_wifi_and_connect_linux
+    fi
+		is_wifi_on
+		if [[ $? -eq 1 ]]; then
+			println_error "Failed to turn on Wi-Fi."
+		fi
+	fi
+
+	if is_internet_connectable; then
+		println_ok "Successfully connected to the Internet."
+	else
+		if [[ $time_limited_account -eq 0 ]]; then
+			println_info "Time limited account. Checking the time..."
+			if check_time; then
+				println_ok "Time is within the range."
+				login_the_wifi
+				is_internet_connectable
+				if [[ $? -eq 1 ]]; then
+					println_error "Failed to connect to the Internet."
+				fi
+			else
+				println_error "Time is out of range."
+				exit 1
+			fi
+		fi
+	fi
+}
+
+# init options
+# must be done before calling main
+while getopts ':i:o:t:p:mnh' OPT; do
+	case $OPT in
+	i) interface="$OPTARG" ;;
+	o) operator="$OPTARG" ;;
+	t) timeout="$OPTARG" ;;
+	p) ipv4_addr="$OPTARG" ;;
+	m) logout_flag=0 ;;          # mode
+	n) time_limited_account=1 ;; # not time limited
+	h) help ;;
+	:)
+		println_error "Option -$OPTARG requires an argument."
+		exit 1
+		;;
+	\?) help ;;
+	esac
+done
+shift $((OPTIND - 1))
+login_id=$1
+login_pw=$2
+if [[ -z $login_id ]] || [[ -z $login_pw ]]; then
+	if [[ $logout_flag -ne 0 ]]; then
+		println_error "Please specify the user ID and password."
+		help
+	fi
+fi
+main
